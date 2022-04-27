459a69d7fae9a672a40ace8eae3c4a81c4ab3c79
VadyChel <zyzel19@gmail.com>
2022-04-27 12:09:23 +0300

    First commit

---
+++ plugins/sensible.js
@@ -0,0 +1,13 @@
+import fastifyPlugin from 'fastify-plugin'
+import fastifySensible from "fastify-sensible"
+
+/**
+ * This plugins adds some utilities to handle http errors
+ *
+ * @see https://github.com/fastify/fastify-sensible
+ */
+export default fastifyPlugin(async (fastify, opts) => {
+  fastify.register(fastifySensible, {
+    errorHandler: false
+  })
+})
