b7420ec4f9c7a94214e7e314e348e15893b0a42b
VadyChel <zyzel19@gmail.com>
2022-04-28 20:54:18 +0300

    feat: remove auto populate, add auto position for page schema

--- models/notes.js
+++ models/notes.js
@@ -3,22 +3,13 @@ import mongoose from 'mongoose'
 const NotesSchema = new mongoose.Schema({
   page: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Page' },
   content: { type: String, required: true },
-  styles: { type: Object, default: {} },
+  styles: { type: Object, default: {someK: 'someV'} },
   position: {
     type: Number,
     required: true
   },
   author: { type: String, required: true }
 }, { collection: 'notes' })
-
-function autoPopulate (next) {
-  this.populate('page')
-  next()
-}
-
-NotesSchema
-  .pre('findOne', autoPopulate)
-  .pre('find', autoPopulate)
 
 const Note = mongoose.model('Note', NotesSchema)
 export default Note
--- models/pages.js
+++ models/pages.js
@@ -1,14 +1,21 @@
 import mongoose from 'mongoose'
 
 const PagesSchema = new mongoose.Schema({
-  root: { type: Boolean, default: false },
+  root: { type: Boolean, default: true },
   parent: { type: String, default: null },
   nestedPages: [Number],
-  position: {
-    type: Number,
-    required: true
-  },
+  position: { type: Number, default: 0 },
+  author: { type: String },
   name: { type: String, required: true }
 }, { collection: 'pages' })
+
+async function autoPosition(next) {
+  const currentAuthorPages = await Page.find({ author: this.author }).sort('-position')
+  this.position = currentAuthorPages[0].position+1
+  next()
+}
+
+PagesSchema.pre('save', autoPosition)
+
 const Page = mongoose.model('Page', PagesSchema)
 export default Page
