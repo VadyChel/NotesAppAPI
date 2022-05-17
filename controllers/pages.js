f058ad4acf2e219a4dd5c329520c38ba34de3ae2
VadyChel <zyzel19@gmail.com>
2022-05-17 11:48:46 +0300

    refactor: change pages controller and service methods

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,31 +1,20 @@
-import Page from '../database/models/pages.js'
-import ForbiddenError from '../exceptions/forbiddenError.js'
-import NotFoundError from '../exceptions/notFoundError.js'
+import PagesService from '../services/pages.js'
 
 class PagesController {
   async getUserPages(req, rep) {
-    return await Page.find({ author: req.currentUser.userId })
+    return await PagesService.getUserPages(req.currentUser.userId)
   }
 
   async createPage(req, rep) {
-    return await Page.create({ name: req.body.name, author: req.currentUser.userId })
+    return await PagesService.createPage({ name: req.body.name, author: req.currentUser.userId })
   }
 
   async deletePage(req, rep) {
-    const foundPage = await Page.findOne({ _id: req.params.pageId })
-    if(!foundPage) throw new NotFoundError('Page not found')
-    if(foundPage.author !== req.currentUser.userId) throw new ForbiddenError()
-
-    await Page.deleteOne({ _id: req.params.pageId })
-    return { success: true }
+    return await PagesService.deletePage(req.params.pageId, req.currentUser.userId)
   }
 
   async updatePage(req, rep) {
-    const foundPage = await Page.findOne({ _id: req.params.pageId })
-    if(!foundPage) throw new NotFoundError('Page not found')
-    if(foundPage.author !== req.currentUser.userId) throw new ForbiddenError()
-
-    return await Page.updateOne({ _id: req.params.pageId }, req.body.page)
+    return await PagesService.updatePage(req.params.pageId, req.body.newPage, req.currentUser.userId)
   }
 }
 
--- services/pages.js
+++ services/pages.js
@@ -1,20 +1,43 @@
 import Page from '../database/models/pages.js'
+import NotFoundError from '../exceptions/notFoundError.js'
+import BadRequestError from '../exceptions/badRequestError.js'
+import ForbiddenError from '../exceptions/forbiddenError.js'
 
 class PagesService {
-  async editPage(pageId, newPage) {
+  async updatePage(pageId, newPage, userId) {
+    const foundPage = await this.getPage(pageId)
+    if(foundPage.author !== userId) throw new ForbiddenError()
     await Page.updateOne({ _id: pageId }, newPage)
+
+    return { success: true }
   }
 
   async createPage(page) {
     return await Page.create(page)
   }
 
-  async deletePage(pageId) {
-    await Page.deleteOne({ _id: pageId })
+  async deletePage(pageId, userId) {
+    const foundPage = await this.getPage(pageId)
+    if(foundPage.author !== userId) throw new ForbiddenError()
+    await Page.deleteOne({ pageId })
+
+    return { success: true }
   }
 
   async getUserPages(userId) {
     return await Page.find({ author: userId })
+  }
+
+  async getPage(pageId) {
+    let foundPage
+    try {
+      foundPage = await Page.findOne({ _id: pageId })
+    } catch(e) {
+      throw new BadRequestError('Invalid page id')
+    }
+
+    if(!foundPage) throw new NotFoundError('Page not found')
+    return foundPage
   }
 }
 
