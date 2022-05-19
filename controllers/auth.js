64320f67f76fa66853be4c82cc5443dc7c291833
VadyChel <zyzel19@gmail.com>
2022-05-19 22:12:22 +0300

    fix: fix refresh token cookie max age

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 import AuthService from '../services/auth.js'
 
 class AuthController {
+  MAX_REFRESH_TOKEN_COOKIE_AGE = this.MAX_REFRESH_TOKEN_COOKIE_AGE
+
   async register(req, rep) {
     const { username, password, email } = req.body
     const userData = await AuthService.register(password, email, username)
     return rep.setCookie('refreshToken', userData.tokens.refreshToken, {
-      maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true
+      maxAge: this.MAX_REFRESH_TOKEN_COOKIE_AGE, httpOnly: true
     }).send(userData)
   }
 
@@ -13,7 +15,7 @@ class AuthController {
     const { password, email } = req.body
     const userData = await AuthService.login(password, email)
     return rep.setCookie('refreshToken', userData.tokens.refreshToken, {
-      maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true
+      maxAge: this.MAX_REFRESH_TOKEN_COOKIE_AGE, httpOnly: true
     }).send(userData)
   }
 
@@ -21,7 +23,7 @@ class AuthController {
     const refreshToken = req.cookies.refreshToken
     const userData = await AuthService.refresh(refreshToken)
     return rep.setCookie('refreshToken', userData.tokens.refreshToken, {
-      maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true
+      maxAge: this.MAX_REFRESH_TOKEN_COOKIE_AGE, httpOnly: true
     }).send(userData)
   }
 
