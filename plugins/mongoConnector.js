5c1354a7db408c984bdb481c9a3895e991ee9b66
VadyChel <zyzel19@gmail.com>
2022-09-29 19:25:44 +0300

    fix: mongoose connection error fixed

--- plugins/mongoConnector.js
+++ plugins/mongoConnector.js
@@ -1,16 +1,16 @@
 import fastifyPlugin from 'fastify-plugin'
 import mongoose from 'mongoose'
 
-export default fastifyPlugin(async (fastify, options) => {
+export default fastifyPlugin((fastify, options, done) => {
   mongoose.connection.on('connect', () => {
     fastify.log.info(`MongoDB connected with user ${process.env.MONGO_USERNAME} on ${process.env.MONGO_DATABASE} database`)
   })
   mongoose.connection.on('disconnect', () => {
     fastify.log.info('MongoDB disconnected')
   })
-  try {
-    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`)
-  } catch(e) {
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`).catch((e) => {
     fastify.log.error(`Mongo error: ${e}`)
-  }
+  })
+
+  done()
 })
